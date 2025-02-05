234e2c2f3f355f22123413e403d4bad89a1d4643
russellkwong <67924002+russellkwong@users.noreply.github.com>
2025-02-05 01:19:01 -0500

    Update hike-template.py
    reformatted code

--- hike-template.py
+++ hike-template.py
@@ -83,12 +83,63 @@ def MakeRec_LL(llx, lly, w, h):
     rec = ap.Polygon(array)
     return rec
 
+def lyr_remove(m, lyr):
+    """
+    Attemptes to remove a layer from the Map.
+
+    Parameters:
+    m (map object): The map containing the layer.
+    lyr (layer object): The layer to be removed.
+
+    Returns:
+    None
+    """
+    try:
+        lyrname = lyr.name
+        m.removeLayer(lyr)
+        print(f'Layer \'{lyrname}\' removed')
+    except:
+        print('Layer not found')
+    pass
+
+color_dict = {'grey10': [25, 25, 25],
+              'grey20': [51, 51, 51],
+              'grey30': [76, 76, 76],
+              'grey40': [102, 102, 102],
+              'grey50': [127, 127, 127],
+              'grey60': [153, 153, 153],
+              'grey70': [178, 178, 178],
+              'grey80': [204, 204, 204],
+              'grey90': [229, 229, 229],
+              'grey100': [255, 255, 255]
+             }
+
+def color_builder(color, alpha):
+    """
+    Creates a dictionary for the defined color and transparency.
+
+    Parameters:
+    color (str): Name of color in the color dictionary.
+    alpha (float): Transparency value.
+
+    Returns:
+    Dictionary of the RGBa color.
+    """
+    color_exp = []
+    color_exp = color_dict[color]
+    color_exp.append(alpha)
+    return {'RGB': color_exp}
+
 # PROJECT CODE
 m = map_obj('Map')
 
 ## Setting Directories
 aprx_dir = r'C:\Users\kwong\Desktop\best-hikes\SpatialFiles'
 aprx_gdb = r'C:\Users\kwong\Desktop\best-hikes\MyProject.gdb'
+
+# Turn off basemap
+lyr = lyr_obj(m, 'Topographic')
+lyr.visible = False
 
 # TRACKS
 ap.conversion.GPXtoFeatures(
@@ -110,14 +161,24 @@ lyr = ap.management.PointsToLine(
 
 lyr = lyr_obj(m, 'hike_routes_tracks')
 
+# BEST HIKES ROUTES
 sym = lyr.symbology
 
 sym.renderer.symbol.outlineWidth = 2.5
 sym.renderer.symbol.outlineColor = {'RGB': [52, 52, 52, 60]}
 lyr.symbology = sym
 
+m.addDataFromPath(os.path.join(aprx_gdb, r'besthikes_routes'))
+lyr = lyr_obj(m, 'besthikes_routes')
+
+sym = lyr.symbology
+sym.renderer.symbol.outlineWidth = 4
+sym.renderer.symbol.outlineColor = color_builder('grey20', 30)
+lyr.symbology = sym
+
 # LAND COVER
 def gen_fields():
+    
     nlcd = m.addDataFromPath(r'https://www.arcgis.com/home/item.html?id=3ccf118ed80748909eb85c6d262b426f')
     
     temp_buff = ap.analysis.PairwiseBuffer(
@@ -165,13 +226,54 @@ sym.renderer.fields = ['gridcode']
 for grp in sym.renderer.groups:
     for itm in grp.items:
         if itm.values[0][0] in ['71', '81']:
-            itm.symbol.applySymbolFromGallery('10% Ordered Stipple')        
+            itm.symbol.applySymbolFromGallery('10% Ordered Stipple')
+            # Need to make shite background transparent
+            # itm.symbol.color = {'RGB': [255, 255, 255, 0]}
 #         elif itm.values[0][0] in ['11', '90', '95']:
 #             itm.symbol.applySymbolFromGallery('10% Simple Hatch')
         else:
             itm.symbol.color = {'RGB': [255, 255, 255, 0]}
         itm.symbol.outlineWidth = 0
 lyr.symbology = sym
+
+lyrlist = ['route_buffer',
+           'USA NLCD Land Cover']
+
+for lyr_name in lyrlist:
+    lyr = lyr_obj(m, lyr_name)
+    lyr_remove(m, lyr)
+
+# HILLSHADE
+def createHillshade(ocs, ext, gdb, lp):
+    if lp is True:
+        with arcpy.EnvManager(outputCoordinateSystem = ocs,
+                              extent = (ext + ocs)):
+            arcpy.ddd.HillShade(
+                in_raster="County_Tompkins2008_2_meter",
+                out_raster=os.path.join(gdb, r'HillSha_Coun1'),
+                azimuth=315,
+                altitude=45,
+                model_shadows="NO_SHADOWS",
+                z_factor=1
+            )
+
+# ext_lp1 - close range (extreme hills)
+# ext_lp2 - far range (less hills)
+# ext_lp3 - med range (med hills)
+
+ocs = 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],VERTCS["WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PARAMETER["Vertical_Shift",0.0],PARAMETER["Direction",1.0],UNIT["Meter",1.0]]'
+ext_lp1 = '-76.5271191647879 42.3051024869337 -76.5054331313753 42.3211945965332 '
+ext_lp2 = '-76.5306704765596 42.3004776482477 -76.4988311322597 42.3258859175778 '
+ext_lp3 = '-76.525825108996 42.3054403361241 -76.5044214385469 42.3202781463147 '
+
+createHillshade(ocs, ext_lp2, aprx_gdb, True)
+
+lyr = lyr_obj(m, 'HillSha_Coun1')
+lyr_rename(lyr, 'topo')
+sym = lyr.symbology
+sym.colorizer.gamma = 2.0
+lyr.symbology = sym
+lyr.transparency = 10
 
 # CONTOURS
 lyr = m.addDataFromPath(os.path.join(aprx_dir, r'TompCty_Contours\Tompkins_County_Natural_Resources_Inventory_(OLD).shp'))
@@ -210,10 +312,11 @@ lyr_rename(lyr, 'hydro')
 sym = lyr.symbology
 sym.updateRenderer('SimpleRenderer')
 
-sym.renderer.symbol.color = {'RGB': [201, 201, 201, 100]}
+sym.renderer.symbol.color = {'RGB': [153, 153, 153, 100]}
 sym.renderer.symbol.outlineWidth = 1
-sym.renderer.symbol.outlineColor = {'RGB': [178, 178, 178, 100]}
+sym.renderer.symbol.outlineColor = {'RGB': [51, 51, 51, 100]}
 lyr.symbology = sym
+# Need to add labels to water bodies
 
 # STREAMS
 lyr = m.addDataFromPath(r'https://gisservices.its.ny.gov/arcgis/rest/services/NYS_Hydrography/MapServer/15',
@@ -224,8 +327,8 @@ lyr_rename(lyr, 'streams')
 sym = lyr.symbology
 sym.updateRenderer('SimpleRenderer')
 
-sym.renderer.symbol.color = {'RGB': [178, 178, 178, 100]}
-sym.renderer.symbol.outlineWidth = 1
+sym.renderer.symbol.color = {'RGB': [153, 153, 153, 100]}
+sym.renderer.symbol.outlineWidth = 2
 lyr.symbology = sym
 
 if lyr.supports('SHOWLABELS'):
@@ -233,6 +336,7 @@ if lyr.supports('SHOWLABELS'):
     lbl_cim = lblClass.getDefinition('V3')
     lbl_cim.textSymbol.symbol.symbol.symbolLayers[0].color.values = [52, 52, 52, 100]
     lblClass.setDefinition(lbl_cim)
+# Need to reformat labels
 
 
 # ROADS
@@ -241,13 +345,42 @@ lyr = m.addDataFromPath(r'https://gisservices.its.ny.gov/arcgis/rest/services/NY
                  custom_parameters = {})
 lyr_rename(lyr, 'roads')
 
+lyr = lyr_obj(m, 'roads')
 sym = lyr.symbology 
 sym.updateRenderer('SimpleRenderer')
 
-sym.renderer.symbol.applySymbolFromGallery('Minor Road')
+symb = sym.renderer.symbol.listSymbolsFromGallery('Minor Road')[1]
+sym.renderer.symbol = symb
 lyr.symbology = sym
 
+classList = ['Label Class 3', # state highway no
+            'Label Class 4',  # county highway lbl
+            'Label Class 5', # state highway lbl
+            'Label Class 6']  # county route no
+
+# if lyr.supports('SHOWLABELS'):
+#     for lblClassName in classList:
+#         lblClass = lyr.listLabelClasses(lblClassName)[0]
+#         lbl_cim = lblClass.getDefinition('V3')
+# #         lbl_cim.textSymbol.symbol.symbol.symbolLayers[0].color.values = [52, 52, 52, 100]
+#         if lblClassName == 'Label Class 3':
+#              # print(dir(lbl_cim.textSymbol.symbol.callout.pointSymbol.symbolLayers[0]))
+# #             print((lbl_cim.textSymbol.symbol.callout.pointSymbol.symbolLayers[0].size))
+# #         lblClass.setDefinition(lbl_cim)
+# Need to find label and callout class for roads
+
 # POI
+aprx_styl = r"C:\Users\kwong\Desktop\best-hikes\styles"
+
+def addStyle(styl_path):
+    styleItemList = aprx.styles
+    if not styl_path in aprx.styles:
+        styleItemList.append(styl_path)
+        aprx.updateStyles(styleItemList)
+    pass
+
+addStyle(os.path.join(aprx_styl, r'Government.stylx'))
+
 ap.management.XYTableToPoint(
     in_table = os.path.join(aprx_dir, r'POI_hikes_18Jan25.csv'),
     out_feature_class = os.path.join(aprx_gdb, r'POI_hikes'),
@@ -257,14 +390,23 @@ ap.management.XYTableToPoint(
     coordinate_system='GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]];-400 -400 1000000000;-100000 10000;-100000 10000;8.98315284119521E-09;0.001;0.001;IsHighPrecision'
 )
 
-poi_symbols = {'Bus stop': 'Bus',
-              'Geology': 'Mountain', 
-              'Historic': 'Museum',
-              'Lean-to': 'Military Base',
-              'Parking': 'Parking',
-              'Trailhead': 'Trail',
-              'Viewpoint': 'Landmark',
-              'Waterfall': 'Cemetery'}
+poi_symbols = {'Bus stop': {'icon': 'Mass Transit',
+                            'index': 0},
+              'Geology': {'icon': 'Climbing',
+                          'index': 0},
+              'Historic': {'icon': 'Museum',
+                           'index': 1},
+              'Lean-to': {'icon': 'Shelter',
+                          'index': 1},
+              'Parking': {'icon': 'Parking',
+                          'index': 4},
+              'Trailhead': {'icon': 'Trailhead',
+                            'index': 0},
+              'Viewpoint': {'icon': 'View',
+                            'index': 2},
+              'Waterfall': {'icon': 'Waterfall',
+                           'index': 0}
+              }
 
 lyr = lyr_obj(m, 'POI_hikes')
 sym = lyr.symbology
@@ -273,11 +415,10 @@ sym.updateRenderer('UniqueValueRenderer')
 sym.renderer.fields = ['type']
 for grp in sym.renderer.groups:
     for itm in grp.items:
-        symb_list = itm.symbol.listSymbolsFromGallery(poi_symbols[itm.values[0][0]])
-        for symb in symb_list:
-            if symb.size == 20:
-                itm.symbol = symb
-        itm.symbol.color = {'RGB': [32, 32, 32, 100]}
+        symb_name = poi_symbols[itm.values[0][0]]['icon']
+        symb_index = poi_symbols[itm.values[0][0]]['index']
+        symb = itm.symbol.applySymbolFromGallery(symb_name, symb_index)
+        # itm.symbol = symb
         itm.symbol.size = 12
 lyr.symbology = sym
 
@@ -304,9 +445,9 @@ lyr = lyr_obj(m, 'flltPreserve')
 sym = lyr.symbology
 sym.updateRenderer('SimpleRenderer')
 
-sym.renderer.symbol.applySymbolFromGallery('Dashed')
-sym.renderer.symbol.outlineWidth = 1.5
-sym.renderer.symbol.outlineColor = {'RGB': [100, 100, 100, 60]}
+sym.renderer.symbol.applySymbolFromGallery('Extent Transparent Gray')
+# sym.renderer.symbol.outlineWidth = 1.5
+# sym.renderer.symbol.outlineColor = {'RGB': [100, 100, 100, 60]}
 lyr.symbology = sym
 
 lyr = ap.conversion.JSONToFeatures(
@@ -368,62 +509,3 @@ sym = lyr.symbology
 sym.renderer.symbol.outlineWidth = 3.4
 sym.renderer.symbol.outlineColor = {'RGB': [52, 52, 52, 60]}
 lyr.symbology = sym
-
-## Add topo
-m.addDataFromPath(r'https://elevation.its.ny.gov/arcgis/rest/services/County_Tompkins2008_2_meter/ImageServer',
-                 web_service_type = 'ARCGIS_SERVER_WEB',
-                 custom_parameters = {})
-ext_lp = '-76.5271191647879 42.3051024869337 -76.5054331313753 42.3211945965332 GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],VERTCS["WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PARAMETER["Vertical_Shift",0.0],PARAMETER["Direction",1.0],UNIT["Meter",1.0]]'
-scrWksp = aprx_gdb
-
-def createHillshade(ext, scrWksp, lp):
-    if lp is True:
-        with ap.EnvManager(extent = ext_lp,
-                          scratchWorkspace = aprx_gdb):
-            out_raster = ap.sa.Hillshade(
-                in_raster="County_Tompkins2008_2_meter",
-                azimuth=315,
-                altitude=45,
-                model_shadows="NO_SHADOWS",
-                z_factor=1
-            )
-            out_raster.save(os.path.join(aprx_gdb, r'HillSha_Coun1'))
-    pass
-
-createHillshade(ext_lp, scrWksp, True)
-
-lyr = lyr_obj(m, 'out_raster')
-lyr_rename(lyr, 'topo')
-
-sym = lyr.symbology
-sym.colorizer.gamma = 3.0
-lyr.symbology = sym
-
-## Road label config
-# lyr = m.addDataFromPath(r'https://gisservices.its.ny.gov/arcgis/rest/services/NYS_Streets/MapServer/7',
-#                  web_service_type = 'ARCGIS_SERVER_WEB',
-#                  custom_parameters = {})
-# lyr_rename(lyr, 'roads')
-
-# lyr = lyr_obj(m, 'roads')
-# sym = lyr.symbology 
-# sym.updateRenderer('SimpleRenderer')
-
-# symb = sym.renderer.symbol.listSymbolsFromGallery('Minor Road')[1]
-# sym.renderer.symbol = symb
-# lyr.symbology = sym
-
-classList = ['Label Class 3', # state highway no
-            'Label Class 4',  # county highway lbl
-            'Label Class 5', # state highway lbl
-            'Label Class 6']  # county route no
-
-if lyr.supports('SHOWLABELS'):
-    for lblClassName in classList:
-        lblClass = lyr.listLabelClasses(lblClassName)[0]
-        lbl_cim = lblClass.getDefinition('V3')
-#         lbl_cim.textSymbol.symbol.symbol.symbolLayers[0].color.values = [52, 52, 52, 100]
-        if lblClassName == 'Label Class 3':
-             print(dir(lbl_cim.textSymbol.symbol.callout.pointSymbol.symbolLayers[0]))
-#             print((lbl_cim.textSymbol.symbol.callout.pointSymbol.symbolLayers[0].size))
-#         lblClass.setDefinition(lbl_cim)
